fa4fe89b7cb966f9b50a490b48c17f0ab184dc27
liuzelei <liuzelei@gmail.com>
2012-05-05 01:08:06 +0800

    update dev server port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ WetongjiWeb::Application.configure do
   # if mail can't send, just ignore it
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => '106.187.95.107:3000' }
+  config.action_mailer.default_url_options = { :host => '106.187.95.107:8080' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
