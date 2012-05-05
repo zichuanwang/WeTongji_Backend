64b4d2be58e173f9f5e2a54cc0902ab4a73cfe79
liuzelei <liuzelei@gmail.com>
2012-05-06 02:31:16 +0800

    for imagemagic

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,4 +1,6 @@
 class Organizer < ActiveRecord::Base
 	has_many :activities
 	has_many :sub_organizers
+
+	#has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ WetongjiWeb::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  Paperclip.options[:command_path] = "/usr/bin/"
+  
   # email setting
   # if mail can't send, just ignore it
   config.action_mailer.perform_deliveries = true
