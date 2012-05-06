4493dd74f70f85d479c38d7ba02196555e792154
liuzelei <liuzelei@gmail.com>
2012-05-06 16:38:59 +0800

    api return status mapping

--- app/models/api_return_status.rb
+++ app/models/api_return_status.rb
@@ -1,31 +1,23 @@
 # encoding: utf-8
 class ApiReturnStatus
   attr_reader :Id, :Memo
+
+  STATUS = {
+    "000" => "成功。",
+    "001" => "参数无法通过校验。",
+    "002" => "方法无效。",
+    "003" => "参数无效。",
+    "004" => "缺少必要的系统参数。",
+    "005" => "未获得权限，请登录。",
+    "006" => "登录超时，请重新登录。",
+    "007" => "缺少方法必须的参数。",
+    "010" => "密码不符合规范。",
+    "999" => "未知错误。"
+  }
   
   def initialize(id) 
     @Id = id
-    case id
-      when "000"
-        @Memo = "成功。"
-      when "001"
-        @Memo = "参数无法通过校验。"
-      when "002"
-        @Memo = "方法无效。"
-      when "003"
-        @Memo = "参数无效。"
-      when "004"
-        @Memo = "缺少必要的系统参数。"
-      when "005"
-        @Memo = "未获得权限，请登录。"
-      when "006"
-        @Memo = "登录超时，请重新登录。"
-      when "007"
-        @Memo = "缺少方法必须的参数。"
-      when "010"
-        @Memo = "密码不符合规范。"
-      when "999"
-        @Memo = "UnKnown"
-    end
+    @Memo = STATUS[id]
   end
   
 end
