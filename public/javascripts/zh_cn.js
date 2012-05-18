67fead376cc71ed1799a127c47d260b93f16288e
liuzelei <liuzelei@gmail.com>
2012-05-19 01:24:56 +0800

    update index and new page

---
+++ public/javascripts/zh_cn.js
@@ -0,0 +1,147 @@
+jquery_lang_js.prototype.lang.zh_cn = {
+
+	'Download':
+	'下载',
+	
+	'Features':
+	'功能',
+	
+	'About Us':
+	'关于我们',
+	
+	'Smart and Revolutionary':
+	'iOS校园信息全新体验',
+	
+	'Requirements: iOS 5.0 or higher(iPhone)':
+	'运行环境：iOS 5.0 或更高版本(iPhone)',
+	
+	'follow Snip':
+	'收听 Snip 官方微博',
+	
+	'Integration of Campus News':
+	'校园信息整合发布',
+	
+	'Important News, Activities and Shows, Academic Lectures, Sports Events.':
+	'重大新闻、活动演出、学术讲座、体育赛事、实习招聘......',
+	
+	'All the campus information is published on the unified platform, keeping you in the front line of every wonderful moment happening on campus without bother by endless messages.':
+	'校园信息同一平台整合发布，不让你再错过任何一个学校精彩的瞬间，不再为没完没了的短信和飞信通知而烦恼。',
+	
+	'Real-time Check of Events':
+	'实时查看管理内容',
+	
+	'WeTongji is not only a campus mobile media but also a helpful tool of managing your campus news and information! Keep in touch with the latest news happening among your campus-mates, and arrange your own fancy events whenever you want.':
+	'微同济不仅仅是校园移动媒体，更是一个校园信息管理工具！把实时校园信息装进口袋，随时随地地查看并管理自己感兴趣的内容。',
+	
+	'Brand-new Campus Life':
+	'全新校园生活模式',
+	
+	'Whether you are the common WEer or the artistic WEer, even the so-called XX WEer, you could finally find your own life style in WeTongji with the best experience mixed with varieties of fun or excitement.':
+	'不管你是普通青年还是文艺青年又或者是XX青年，都能在微同济找到属于你自己的全新校园生活模式，体验大学的种种乐趣和缤纷。',
+	
+	'Is it possible for WeTongji to publish on Android platform?':
+	'WeTongji 会支持Android平台吗？',
+	
+	'Of course, the version of Android will be released around the beginning of next semester, functioning the same as that on iOS in overall.':
+	'当然会，我们将在下学期初发布Android的版本，功能将与iOS的版本大致保持一致。',
+	
+	'How does WeTongji get the information of users?Is it safe?':
+	'微同济是如何获取用户信息，安全吗？',
+	
+	'WeTongji gets parts of the information from Academic Affairs of Tongji University each term，and uploads it to the server .Our operations team will affix the Confidentiality agreement and maintain the data under the supervision of the advisor of the Wetongji.':
+	'微同济每学期都会从学校教务获取学生部分信息的副本并加载到服务器上。微同济的运营团队都会签署数据保密协议，并在指导老师的监管下维护数据。',
+	
+	'Note: To restore to the default shortcut, place the cursor in input box of shortcut and click Delete.':
+	'注：光标落在快捷键的输入框时，点击delete键，即可恢复默认的快捷键。',
+	
+	'Is the information of activities published via WeTongji reliable?':
+	'微同济上的活动信息可靠吗？',
+	
+	'WeTongji is not only an app for spreading information，but also a platform provider.Any information of activity is published by the school organization ，which is certified ，via the WeTongji Admin System.':
+	'微同济不仅是信息传播的移动终端，也是一个平台提供方。所有活动信息都是由获取认证的校园组织通过微同济的后台管理系统发布。',
+	
+	'How could I deal with the information of activities from the channels?':
+	'对于频道中的活动我可以对它做什么操作？',
+	
+	'For the users, you could add the activities both to your favorite and schedule. Even more, you could give a "like".':
+	'你可以收藏频道中的活动，添加至你的个人日程，并且给喜欢的活动好评。',
+	
+	'Will WeTongji develop more new functions？':
+	'微同济在未来还会推出新功能吗？',
+	
+	'Yes，WeTongji will always focus on more practical and more convenient mobile service. Circle and Mail are coming soon in the follow-up version.Any suggestion is favorable to depict the promising future together.':
+	'微同济会一直致力于更实用更方便的校园移动服务，在后续的版本中将会推出圈子和邮箱功能。微同济欢迎大家给我们提出宝贵建议，共同描绘微同济的美好未来。',
+	
+	'How to save my captures?':
+	'截屏后怎么将图片保存下来？',
+	
+	'Double-click in the region you cropped or click save icon':
+	'截取屏幕后，在截取区域内双击光标或点击保存的图标',
+	
+	', the pictures will be saved to the local.':
+	'，即可将图片保存到本地。',
+	
+	'How to move tags?':
+	'怎么才能移动已添加的图形标记？',
+	
+	'Move the cursor over the tag. When the cursor turns into a hand, click to select the tag and move it.':
+	'当光标移动到图形标记上显示为手形时，点击即可选中图形标记，此时便可进行移动操作。',
+	
+	'How to edit tags?':
+	'怎么才能对已添加的图形标记进行大小颜色等的修改？',
+	
+	'Move the cursor over the tag. When the cursor turns into a hand, click to select the tag and edit it.':
+	'当光标移动到图形标记上显示为双箭头或手形时，点击即可选中图形标记，此时可对其进行大小颜色等的修改。',
+	
+	'Note: Brushes and texts can not be zoomed in/out.':
+	'注：画刷和文本涂鸦不支持缩放。',
+	
+	'How to edit annotates?':
+	'怎么修改已添加的文本内容？',
+	
+	'Move the cursor over the text. When it turns into a hand, double-click to edit.':
+	'当光标移动到文本上显示为手形时，双击后可进行编辑。',
+	
+	'If you encountered any problem when using WeTongji, please contact us as following ways:':
+	'若您在使用 WeTongji 中还遇到了其他问题，欢迎通过以下方式和我们联系：',	
+	
+	'Tencent Weibo':
+	'腾讯微博',
+	
+	'Mail to us':
+	'邮件联系',
+	
+	'Install QQmail Plug-in':
+	'安装QQ邮箱浏览器插件',
+	
+	'Features:':
+	'QQ邮箱浏览器插件可以为您带来以下便利：',
+	
+	'Allow online file storage (up to 1G for each file) and support resuming uploads.':
+	'实现中转站快速上传最大1G的单个文件、支持断点续传',
+	
+	'Capture the screen while composing email.':
+	'提供写信时的截屏功能',
+	
+	'Snip, a screen capture software, will be installed as well, providing advanced features to capture screen.':
+	'同时安装的截屏软件 Snip，让您随时可使用到专业的截屏功能',
+	
+	'Remove QQmail Plug-in':
+	'卸载QQ邮箱浏览器插件',
+	
+	'Simplely install the QQmail plugin, you can capture with Snip and easily share your screenshots with friends in QQmail.':
+	'你只需要花费几秒钟时间安装一个浏览器插件，即可在QQ邮箱中使用到Snip提供的专业截屏功能，将截图分享给你的邮件联系人。',
+	
+	'Download the QQmail plugin uninstaller to remove the plugin.':
+	'如果你需要卸载QQ邮箱截屏插件，请下载以下卸载工具运行即可。',
+	
+	'* Before installing the plugin, you should ':
+	'* 在安装QQ邮箱截屏插件之前，须先 ',
+	
+	'install Snip':
+	'安装Snip',
+	
+	'first.':
+	'才可以正常工作。'
+	
+}
