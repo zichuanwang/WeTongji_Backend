05020b1acb88d9e93aab39cf25f705caac8c7858
liuzelei <liuzelei@gmail.com>
2012-05-20 00:27:53 +0800

    temp commit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -22,6 +22,27 @@ class AdminsController < ApplicationController
   end
 
   def new
+    @menu = 'admins'
+    @admin = Admin.new
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @admin }
+    end
+  end
+
+  def create
+    @menu = 'admins'
+    @admin = Admin.new(params[:admin])
+    respond_to do |format|
+      if @admin.save
+        format.html { redirect_to @admin, notice: 'Admin was successfully created.' }
+        format.json { render json: @admin, status: :created, location: @admin }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update_password
