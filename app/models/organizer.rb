4aae522601ceea1ce4a1bff6f0863463c0d3be74
liuzelei <liuzelei@gmail.com>
2012-06-05 02:11:21 +0800

    preview image

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -2,5 +2,5 @@ class Organizer < ActiveRecord::Base
 	has_many :activities
 	has_one :admin
 
-	has_attached_file :icon, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :icon, :styles => { :medium => "200x200>", :thumb => "100x100>" }
 end
