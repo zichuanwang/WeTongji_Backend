e74c81d17b29ed40951a13a5796346f1ddf23c0c
liuzelei <liuzelei@gmail.com>
2012-06-24 05:31:27 +0800

    news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,74 +3,49 @@ class NewsController < ApplicationController
   def index
     @menu = 'news'
     @news = @news.order("id desc").page(params[:page])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @news }
-    end
   end
 
   def show
     @menu = 'news'
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @news }
-    end
   end
 
-  # GET /news/new
-  # GET /news/new.json
   def new
     @menu = 'news'
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @news }
-    end
   end
 
   def edit
     @menu = 'news'
   end
 
-
   def create
     @menu = 'news'
 
     respond_to do |format|
       if @news.save
-        format.html { redirect_to @news, notice: 'News was successfully created.' }
-        format.json { render json: @news, status: :created, location: @news }
+        format.html { redirect_to :action => "index", notice: 'News was successfully created.' }
       else
         format.html { render action: "new" }
-        format.json { render json: @news.errors, status: :unprocessable_entity }
       end
     end
   end
-
 
   def update
     @menu = 'news'
 
     respond_to do |format|
       if @news.update_attributes(params[:news])
-        format.html { redirect_to @news, notice: 'News was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to :action => "index", notice: 'News was successfully updated.' }
       else
         format.html { render action: "edit" }
-        format.json { render json: @news.errors, status: :unprocessable_entity }
       end
     end
   end
-
 
   def destroy
     @news.destroy
 
     respond_to do |format|
-      format.html { redirect_to news_index_url }
-      format.json { head :no_content }
+      format.html { redirect_to :action => "index" }
     end
   end
 end
