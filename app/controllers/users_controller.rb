e7d6ddce708d6d717c57ef2b93c698164521406e
liuzelei <liuzelei@gmail.com>
2012-07-24 00:03:57 +0800

    user can active account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-  authorize_resource
 	before_filter :authenticate_admin!, :except => [:confirmation, :welcome, :reset_password, :reset_password_success, :reset_password_from_mail]
   def index
-    @users = User.order('id desc')
+    @users = User.accessible_by(current_ability).order('id desc')
     if (params[:type] == "unconfirmed")
       @users = @users.where("confirmed_at is null")
     end
@@ -14,11 +13,6 @@ class UsersController < ApplicationController
     end
     @users = @users.page(params[:page])
     @menu = "users"
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
   end
 
   def reconfirm
@@ -39,7 +33,9 @@ class UsersController < ApplicationController
   end
 
   def reset_password_from_mail
-    @user = User.find_by_reset_password_token(params[:token])
+    unless params[:token].nil?
+      @user = User.find_by_reset_password_token(params[:token])
+    end
     render :layout => "out"
   end
 
@@ -73,76 +69,5 @@ class UsersController < ApplicationController
 
   def welcome
   	render :layout => "out"
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
   end
 end
