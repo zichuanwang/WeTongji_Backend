6ddb7ef9b69f83bbefbfb9af248524550e74682f
liuzelei <liuzelei@gmail.com>
2012-09-06 01:03:05 +0800

    raise error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ WetongjiWeb::Application.configure do
   # email setting
   # if mail can't send, just ignore it
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'we.tongji.edu.cn' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
