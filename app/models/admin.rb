223baff567b2527f49bd2a6085960809f6ef5606
liuzelei <liuzelei@gmail.com>
2012-10-08 23:20:42 +0800

    register success

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -7,7 +7,12 @@ class Admin < ActiveRecord::Base
 
   	# Setup accessible (or protected) attributes for your model
   	attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :title, :display, :phone, 
-  					:description, :name, :address, :student_no
-	has_attached_file :icon, :styles => { :medium => "180x180>", :thumb => "100x100>" }
+  					:description, :name, :address, :student_no, :icon
+	has_attached_file :icon, :styles => { :medium => "180x180>" }
   	has_one :organizer
+
+	validates_presence_of :name, :student_no, :display, :address, :phone, :description
+	validates_uniqueness_of :name, :student_no, :display, :phone
+	validates_length_of :description, :within => 0..200
+	validates_attachment_presence :icon
 end
