ba1ff8bff725a08b1744afc48060eb4ad7df67c9
liuzelei <liuzelei@gmail.com>
2012-11-08 00:21:14 +0800

    add person and person image

---
+++ app/models/person_image.rb
@@ -0,0 +1,2 @@
+class PersonImage < ActiveRecord::Base
+end
---
+++ test/unit/person_image_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PersonImageTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
