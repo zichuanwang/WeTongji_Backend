b6397c8dfb85f5b3ffd65d6c7547361c3d830b4e
liuzelei <liuzelei@gmail.com>
2012-11-07 17:49:46 +0800

    update for non paperclip

--- db/migrate/20121106114629_create_information_images.rb
+++ db/migrate/20121106114629_create_information_images.rb
@@ -2,7 +2,10 @@ class CreateInformationImages < ActiveRecord::Migration
   def change
     create_table :information_images do |t|
     	t.integer :information_id
-    	t.attachment :file
+    	t.string :file_file_name
+    	t.string :file_content_type
+    	t.integer :file_file_size
+    	t.datetime :file_updated_at
       	t.timestamps
     end
   end
