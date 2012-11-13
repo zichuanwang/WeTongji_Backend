0c5f141afbb1c1c24c090e8879ba179b4ffd56ae
liuzelei <liuzelei@gmail.com>
2012-11-14 04:03:31 +0800

    around ui

--- app/controllers/arounds_controller.rb
+++ app/controllers/arounds_controller.rb
@@ -1,83 +1,81 @@
 class AroundsController < ApplicationController
-  # GET /arounds
-  # GET /arounds.json
+  load_and_authorize_resource
   def index
-    @arounds = Around.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @arounds }
-    end
+    @arounds = @arounds.order("id desc").page(params[:page])
   end
 
-  # GET /arounds/1
-  # GET /arounds/1.json
   def show
-    @around = Around.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @around }
-    end
   end
 
-  # GET /arounds/new
-  # GET /arounds/new.json
   def new
-    @around = Around.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @around }
-    end
   end
 
-  # GET /arounds/1/edit
   def edit
-    @around = Around.find(params[:id])
   end
 
-  # POST /arounds
-  # POST /arounds.json
+  def approve
+    @around = Around.find_by_id(params[:id])
+    authorize! :approve, @around
+    @around.visiable = true
+    @around.is_pending = false
+    @around.pending_reason = ""
+    @around.save
+    redirect_to :action => "index"
+  end
+
   def create
-    @around = Around.new(params[:around])
+    @around.admin = current_admin
+    @around.check
+
+    if params[:images]
+      params[:images].each do |image|
+        img = AroundImage.new
+        img.file = image
+        @around.around_images << img
+      end
+    end
 
     respond_to do |format|
       if @around.save
         format.html { redirect_to @around, notice: 'Around was successfully created.' }
-        format.json { render json: @around, status: :created, location: @around }
       else
         format.html { render action: "new" }
-        format.json { render json: @around.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /arounds/1
-  # PUT /arounds/1.json
   def update
-    @around = Around.find(params[:id])
+    if params[:images]
+      params[:images].each do |image|
+        img = AroundImage.new
+        img.file = image
+        @around.around_images << img
+      end
+    end
 
+    if params[:delete_images]
+      params[:delete_images].each do |image|
+        img = AroundImage.find(image)
+        @around.around_images.delete(img)
+      end
+    end
+    
     respond_to do |format|
       if @around.update_attributes(params[:around])
+        @around.check
+        @around.save
         format.html { redirect_to @around, notice: 'Around was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @around.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /arounds/1
-  # DELETE /arounds/1.json
   def destroy
-    @around = Around.find(params[:id])
     @around.destroy
 
     respond_to do |format|
-      format.html { redirect_to arounds_url }
-      format.json { head :no_content }
+      format.html { redirect_to :action => "index" }
     end
   end
 end
