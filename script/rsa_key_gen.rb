b718d307a12dc67c2ab0a57ccff5446585b19c7e
liuzelei <liuzelei@gmail.com>
2013-03-11 21:51:53 +0800

    new key

--- script/rsa_key_gen.rb
+++ script/rsa_key_gen.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'openssl'
 
 sys_key = OpenSSL::PKey::RSA::generate(1024)
@@ -11,3 +12,7 @@ file_public.close
 file_private = File.new("#{Rails.root}/config/keys/#{key_name}_private.pem", "w")
 file_private.puts sys_key.to_pem
 file_private.close
+
+file_public_der = File.new("#{Rails.root}/config/keys/#{key_name}_public.der", "w")
+file_public_der.puts sys_key.public_key.to_der.force_encoding("utf-8")
+file_public_der.close
