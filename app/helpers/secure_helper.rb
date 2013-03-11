5795cbaba70e52c05df7eb7c81fc0e90203838e0
刘泽磊 <liuzelei@gmail.com>
2013-03-11 13:01:55 +0800

    Update secure_helper.rb

--- app/helpers/secure_helper.rb
+++ app/helpers/secure_helper.rb
@@ -2,7 +2,7 @@ require 'openssl'
 
 module SecureHelper
 	def Decrypt(message)
-		key = OpenSSL::PKey::RSA.new(File.read("#{Rails.root}/#{Rails.configuration.rsa_private_key}"))
+		key = OpenSSL::PKey::RSA.new(File.read("#{Rails.configuration.rsa_private_key}"))
 		key.private_decrypt(message)
 	end
 end
