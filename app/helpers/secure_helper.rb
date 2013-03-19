6967e11daec45b2a80196a80f30b39684ddc37ec
liuzelei <liuzelei@gmail.com>
2013-03-19 23:42:21 +0800

    remove unused keys

--- app/helpers/secure_helper.rb
+++ app/helpers/secure_helper.rb
@@ -3,6 +3,8 @@ require 'openssl'
 module SecureHelper
 	def Decrypt(message)
 		key = OpenSSL::PKey::RSA.new(File.read("#{Rails.configuration.rsa_private_key}"))
+		p "====="
+		p Base64.decode64(message).length
 		key.private_decrypt(Base64.decode64(message))
 	end
 
--- script/test.rb
+++ script/test.rb
@@ -1,21 +1,27 @@
 require 'openssl'
 
-key = OpenSSL::PKey::RSA.new(1024)
-digest = OpenSSL::Digest::SHA1.new()
+# key = OpenSSL::PKey::RSA.new(1024)
+# digest = OpenSSL::Digest::SHA1.new()
 
-issu = sub = OpenSSL::X509::Name.new()
-sub.add_entry('C', 'JP')
-sub.add_entry('ST', 'Shimane')
-sub.add_entry('CN', 'Ruby Taro')
+# issu = sub = OpenSSL::X509::Name.new()
+# sub.add_entry('C', 'JP')
+# sub.add_entry('ST', 'Shimane')
+# sub.add_entry('CN', 'Ruby Taro')
 
-cer = OpenSSL::X509::Certificate.new()
-cer.not_before = Time.at(0)
-cer.not_after = Time.at(0)
-cer.public_key = key  # <= 接受签署的公匙 
-cer.serial = 1
-cer.issuer = issu
-cer.subject = sub
+# cer = OpenSSL::X509::Certificate.new()
+# cer.not_before = Time.at(0)
+# cer.not_after = Time.at(0)
+# cer.public_key = key  # <= 接受签署的公匙 
+# cer.serial = 1
+# cer.issuer = issu
+# cer.subject = sub
 
-cer.sign(key, digest) # <= 签署用的秘匙和哈希表函数
-print cer.to_text
-#print cer.to_der
+# cer.sign(key, digest) # <= 签署用的秘匙和哈希表函数
+# print cer.to_text
+# #print cer.to_der
+
+
+
+key = OpenSSL::X509::Certificate.new(File.read("#{Rails.root}/config/keys/public.der"))
+
+p Base64.encode64(key.public_key.public_encrypt("2035678941")).gsub(/\n/, '')
