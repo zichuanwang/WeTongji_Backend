99a20fc831eec83fc31b3952e4e760c940c568a3
刘泽磊 <liuzelei@gmail.com>
2013-03-27 12:59:37 +0800

    Update reset_teachers_password.rb

--- script/reset_teachers_password.rb
+++ script/reset_teachers_password.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 User.where("last_seen_at is null and user_type = '职工'").each_with_index do |item, index|
   ps = rand(1000000..9999999).to_s
-	item.password = ps
+  item.password = ps
   item.save
-	UserMailer.new_password(item, ps).deliver
-  	p "import no:#{index} row!"
+  UserMailer.new_password(item, ps).deliver
+  p "import no:#{index} row!"
 end
