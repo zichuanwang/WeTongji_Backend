721b6e1e7096734102ea3d1d362d195c99e79997
liuzelei <liuzelei@gmail.com>
2013-05-20 23:15:22 +0800

    quick fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,19 @@ WetongjiWeb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'we.tongji.edu.cn' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.tongji.edu.cn",
+    :port                 => 25,
+    :domain               => "tongji.edu.cn",
+    :user_name            => 'we',
+    :password             => 'wetongjinew',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+    #:openssl_verify_mode  => 'none'
+    #:tls                  => false
+  }
 end
