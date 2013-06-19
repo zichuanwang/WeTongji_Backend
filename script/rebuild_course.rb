f8688bf1b68b852418d4e2f8f3bdb67ea9b976fe
liuzelei <liuzelei@gmail.com>
2013-06-20 01:03:52 +0800

    update rebuild script

--- script/rebuild_course.rb
+++ script/rebuild_course.rb
@@ -1,38 +1,40 @@
 # encoding: utf-8
 
 # first, generate uno 
-# SeleCourse.select("course_no, begin, end").group("course_no, begin, end").each do |s|
-# 	uno_prefix = ''
-# 	if s["begin"] < '2012/03/01'
-# 		uno_prefix = '1201'
-# 	elsif s["begin"] < '2012/10/01'
-# 		uno_prefix = '1202'
-# 	elsif s["begin"] < '2013/03/01'
-# 		uno_prefix = '1301'
-# 	end
+SeleCourse.select("course_no, begin, end").group("course_no, begin, end").each do |s|
+	uno_prefix = ''
+	if s["begin"] < '2012/03/01'
+		uno_prefix = '1201'
+	elsif s["begin"] < '2012/10/01'
+		uno_prefix = '1202'
+	elsif s["begin"] < '2013/03/01'
+		uno_prefix = '1301'
+	end
 
-# 	course_with_uno = Course.find_by_uno(uno_prefix + s["course_no"])
+	course_with_uno = Course.find_by_uno(uno_prefix + s["course_no"].to_s)
 
-# 	if course_with_uno.nil?
-# 		course = Course.find_by_no(s["course_no"])
-# 		unless course.nil?
-# 			c_new = Course.new
-# 			c_new.no = course.no
-# 			c_new.uno = uno_prefix + course.no
-# 			c_new.name = course.name
-# 			c_new.teacher = course.teacher
-# 			c_new.point = course.point
-# 			c_new.required = course.required
-# 			c_new.hours = course.hours
-# 			c_new.begin = s["begin"]
-# 			c_new.end = s["end"]
-# 			c_new.save
-# 			p uno_prefix + s["course_no"]
-# 		end
-# 	end
+	if course_with_uno.nil?
+		course = Course.find_by_no(s["course_no"].to_s)
+		unless course.nil?
+			c_new = Course.new
+			c_new.no = course.no
+			c_new.uno = uno_prefix + course.no.to_s
+			c_new.name = course.name
+			c_new.teacher = course.teacher
+			c_new.point = course.point
+			c_new.required = course.required
+			c_new.hours = course.hours
+			c_new.begin = s["begin"]
+			c_new.end = s["end"]
+			c_new.save
+			p "uno " + uno_prefix + s["course_no"].to_s
+		end
+	end
+end
 
-# 	Course.delete_all("uno is null")
-# end
+p "uno generate done!"
+#Course.delete_all("uno is null")
+p "all old data clear!"
 
 # second, generate course scetion
 SeleCourse.select("course_no, begin, end, location, week_type, week_day, section_start, section_end").group("course_no, begin, end, location, week_type, week_day, section_start, section_end").each do |s|
@@ -47,12 +49,15 @@ SeleCourse.select("course_no, begin, end, location, week_type, week_day, section
 	section = CourseSection.new
 	section.location = s["location"]
 	section.week_type = s["week_type"]
-	section.course_uno = uno_prefix + s["course_no"]
+	section.course_uno = uno_prefix + s["course_no"].to_s
 	section.week_day = s["week_day"]
 	section.section_start = s["section_start"]
 	section.section_end = s["section_end"]
 	section.save
+	p "section for " + section.course_uno
 end
+
+p "section generate done!"
 
 # third, generate course take
 SeleCourse.select("student_no, course_no, begin, end").group("student_no, course_no, begin, end").each do |s|
@@ -66,8 +71,10 @@ SeleCourse.select("student_no, course_no, begin, end").group("student_no, course
 	end
 
 	take = CourseTake.new
-	take.student_no = s["student_no"]
-	take.course_uno = uno_prefix + s["course_no"]
+	take.student_no = s["student_no"].to_s
+	take.course_uno = uno_prefix + s["course_no"].to_s
 	take.save
-
+	p "course " + take.course_uno + " take!"
 end
+
+p "course take done!"
