06107e429055d9a41d668480f66171e16e60da60
liuzelei <liuzelei@gmail.com>
2013-06-19 21:32:09 +0800

    update migration script

--- db/migrate/20130619130348_add_column_to_courses.rb
+++ db/migrate/20130619130348_add_column_to_courses.rb
@@ -1,11 +1,21 @@
 class AddColumnToCourses < ActiveRecord::Migration
-  def change
+  def up
     add_column :courses, :uno, :string
     add_column :courses, :begin, :datetime
     add_column :courses, :end, :datetime
     remove_column :course_sections, :course_no
-    add_column :course_sections, :course_uno, :string
+    add_column :course_sections, :course_id, :integer
     remove_column :course_takes, :course_no
-    add_column :course_takes, :course_uno, :string
+    add_column :course_takes, :course_id, :integer
+  end
+
+  def down
+    remove_column :courses, :uno
+    remove_column :courses, :begin
+   	remove_column :courses, :end
+    add_column :course_sections, :course_no, :string
+    remove_column :course_sections, :course_uno
+    add_column :course_takes, :course_no, :string
+    remove_column :course_takes, :course_uno
   end
 end
