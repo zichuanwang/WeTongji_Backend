294cae3097c0a858248c6a0815059adc3be69f83
liuzelei <liuzelei@gmail.com>
2013-07-01 00:15:43 +0800

    status

--- app/models/ex_account.rb
+++ app/models/ex_account.rb
@@ -1,5 +1,5 @@
 class ExAccount
-	attr_accessor :Id, :Name, :Description, :Display, :Image, :Like, :Background
+	attr_accessor :Id, :Name, :Description, :Display, :Image, :Like, :Background, :ActivitiesCount, :InformationCount
 
 	def self.init_from_account(account, user = nil)
 		model = ExAccount.new
@@ -10,7 +10,9 @@ class ExAccount
 			model.Description = account.description
 			model.Like = UserLike.get_count("Account", model.Id)
 			model.Image = !account.icon.exists? ? '' : Rails.configuration.host + account.icon.url(:medium)
-			model.Background = !account.background.exists? ? '' : Rails.configuration.host + account.icon.url(:medium)
+			model.Background = !account.background.exists? ? '' : Rails.configuration.host + account.background.url(:medium)
+			model.ActivitiesCount = account.activities.count
+			model.InformationCount = account.information.count
 		end
 
 		model
