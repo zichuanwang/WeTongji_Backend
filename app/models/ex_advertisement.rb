cd2f8a6fce7466f76b9c28f389370f921dd3292f
liuzelei <liuzelei@gmail.com>
2013-07-18 01:22:17 +0800

    bgcolor remove

--- app/models/ex_advertisement.rb
+++ app/models/ex_advertisement.rb
@@ -1,11 +1,10 @@
 class ExAdvertisement
-	attr_accessor :Id, :Title, :Image, :BgColor, :Publisher
+	attr_accessor :Id, :Title, :Image, :Publisher
 
 	def self.init_from_advertisement(advertisement)
 		model = ExAdvertisement.new
 		model.Id = advertisement.id
 		model.Title = advertisement.title
-		model.BgColor = advertisement.bgcolor
 		model.Image = advertisement.image == nil ? '' : Rails.configuration.host + advertisement.image.url(:medium)
 		model.Publisher = advertisement.publisher
 
