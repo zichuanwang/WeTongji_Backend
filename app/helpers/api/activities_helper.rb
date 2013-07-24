0631496c75e5b6064cb88c2f2aa6aabd615bf7ba
liuzelei <liuzelei@gmail.com>
2013-07-24 12:45:05 +0800

    quick fix issus 7

--- app/helpers/api/activities_helper.rb
+++ app/helpers/api/activities_helper.rb
@@ -207,10 +207,7 @@ module Api
 							#find history invites and delete them
 							all = ActivityInvite.where("`from` = :from and `to` = :to", :from => invite.from, :to => invite.to)
 							all.each do |item|
-								notif = Notification.find_by_out_model_name_and_out_id("ActivityInvite", item.id)
-								unless notif.nil?
-									notif.destroy
-								end
+								Notification.delete_all("out_model_name = 'ActivityInvite' and out_id = :id", :id => item.id)
 								item.destroy
 							end
 
