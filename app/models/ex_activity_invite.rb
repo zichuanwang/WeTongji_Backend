4ff5ca1359646b202a14359929c2ae723adcd104
liuzelei <liuzelei@gmail.com>
2013-07-28 01:10:36 +0800

    update for invite

--- app/models/ex_activity_invite.rb
+++ app/models/ex_activity_invite.rb
@@ -3,15 +3,19 @@ class ExActivityInvite
 
 	def self.init_from_activity_invite(activity_invite, user = nil)
 		model = ExActivityInvite.new
-		model.Id = activity_invite.id
-		model.From = activity_invite.from_name
-		model.To = activity_invite.to_name
-		model.SentAt = activity_invite.created_at
-		model.AcceptedAt = activity_invite.accepted_at
-		model.RejectedAt = activity_invite.rejected_at
-		model.UserDetails = ExUser.init_from_user(activity_invite.from_user, user)
-		model.ToUserDetails = ExUser.init_from_user(activity_invite.to_user, user)
-		model.ActivityDetails = ExActivity.init_from_activity(activity_invite.activity, user)
+		
+		unless activity_invite.nil?
+			model.Id = activity_invite.id
+			model.From = activity_invite.from_name
+			model.To = activity_invite.to_name
+			model.SentAt = activity_invite.created_at
+			model.AcceptedAt = activity_invite.accepted_at
+			model.RejectedAt = activity_invite.rejected_at
+			model.UserDetails = ExUser.init_from_user(activity_invite.from_user, user)
+			model.ToUserDetails = ExUser.init_from_user(activity_invite.to_user, user)
+			model.ActivityDetails = ExActivity.init_from_activity(activity_invite.activity, user)
+		end
+
 		model
 	end
 end
--- app/models/ex_friend_invite.rb
+++ app/models/ex_friend_invite.rb
@@ -3,13 +3,18 @@ class ExFriendInvite
 
 	def self.init_from_friend_invite(friend_invite, user = nil)
 		model = ExFriendInvite.new
-		model.Id = friend_invite.id
-		model.From = friend_invite.from_name
-		model.To = friend_invite.to_name
-		model.SentAt = friend_invite.created_at
-		model.AcceptedAt = friend_invite.accepted_at
-		model.RejectedAt = friend_invite.rejected_at
-		model.UserDetails = ExUser.init_from_user(friend_invite.from_user, user)
-		model.ToUserDetails = ExUser.init_from_user(friend_invite.to_user, user)
+		
+		unless friend_invite.nil?
+			model.Id = friend_invite.id
+			model.From = friend_invite.from_name
+			model.To = friend_invite.to_name
+			model.SentAt = friend_invite.created_at
+			model.AcceptedAt = friend_invite.accepted_at
+			model.RejectedAt = friend_invite.rejected_at
+			model.UserDetails = ExUser.init_from_user(friend_invite.from_user, user)
+			model.ToUserDetails = ExUser.init_from_user(friend_invite.to_user, user)
+		end
+
+		model
 	end
 end
