b8f59cb2a4537c3f6675050fa6657ff4799cee45
liuzelei <liuzelei@gmail.com>
2013-09-03 02:01:22 +0800

    update view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,16 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     #current_aility.sign_out
-    p exception.message
     redirect_to new_admin_session_url
   end
 
   def current_ability
   	@current_ability ||= Ability.new(current_admin)
 	end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
 
   protected
 
