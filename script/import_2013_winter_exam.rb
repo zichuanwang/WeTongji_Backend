e39b41db4f6f824f8b08d1b7222aeff0d9e433e4
liuzelei <liuzelei@gmail.com>
2013-12-28 23:51:42 +0800

    update for uno

--- script/import_2013_winter_exam.rb
+++ script/import_2013_winter_exam.rb
@@ -11,7 +11,7 @@ csv = CSV.parse(csv_text, :headers => true)
 csv.each_with_index do |row, index|
 	s = Exam.new
 	s.student_no = row[0]
-	s.course_no = row[1]
+	s.course_uno = "1302#{row[1]}"
 	s.begin = row[3]
 	s.end = row[4]
 	s.location = row[5]
